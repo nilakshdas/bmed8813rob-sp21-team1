3bd71e53c7ac9e7ada58fa8bf5b27b53627ddee2
Nilaksh Das <nilakshdas@gatech.edu>
2021-02-17 22:07:47 -0800

    Add setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="riddbot",
+    version="0.0.0",
+    description="Robotic Intervention for Dirty Disposal",
+    url="http://github.com/nilakshdas/riddbot",
+    author="BMED 8803 - ROB, Spring 2021, Team 1",
+    packages=find_packages(),
+    zip_safe=False,
+)
